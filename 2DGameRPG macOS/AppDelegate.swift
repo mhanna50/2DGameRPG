a15268a8c11d2aaa0275b5e3c2ace0b41ba392b3
mhanna50 <michaelhanna50@gmail.com>
2025-04-22 09:45:47 -0400

    Initial Commit

---
+++ 2DGameRPG macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  2DGameRPG macOS
+//
+//  Created by michael hanna on 4/22/25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
